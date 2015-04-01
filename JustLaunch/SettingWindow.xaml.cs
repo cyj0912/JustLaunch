712e80a1febcc1802bf3c68f77ff898e166d5d33
Toby Chen <chenyanjun912@hotmail.com>
2015-04-01 16:01:40 -0400

    Finish icon settings.

--- JustLaunch/SettingWindow.xaml.cs
+++ JustLaunch/SettingWindow.xaml.cs
@@ -18,6 +18,7 @@ namespace JustLaunch
 {
     /// <summary>
     /// Interaction logic for SettingWindow.xaml
+    /// UGLY ---- Programed by a WPF noob
     /// </summary>
     public partial class SettingWindow : Window
     {
@@ -49,25 +50,35 @@ namespace JustLaunch
         private void ReadSettings()
         {
             ShortcutMgr = new ShortcutManager(true);
-            String Path = ShortcutMgr.Storage["1"].GetPath();
-            SetApplicationIconFromPath(appBtn1, appImg1, appText1, Path);
-            Path = ShortcutMgr.Storage["2"].GetPath();
-            SetApplicationIconFromPath(appBtn2, appImg2, appText2, Path);
+            if (ShortcutMgr.Storage.ContainsKey("1"))
+                SetApplicationIconFromPath(appBtn1, appImg1, appText1, ShortcutMgr.Storage["1"].GetPath());
+            if (ShortcutMgr.Storage.ContainsKey("2"))
+                SetApplicationIconFromPath(appBtn2, appImg2, appText2, ShortcutMgr.Storage["2"].GetPath());
+            if (ShortcutMgr.Storage.ContainsKey("3"))
+                SetApplicationIconFromPath(appBtn3, appImg3, appText3, ShortcutMgr.Storage["3"].GetPath());
+            if (ShortcutMgr.Storage.ContainsKey("4"))
+                SetApplicationIconFromPath(appBtn4, appImg4, appText4, ShortcutMgr.Storage["4"].GetPath());
+            if (ShortcutMgr.Storage.ContainsKey("5"))
+                SetApplicationIconFromPath(appBtn5, appImg5, appText5, ShortcutMgr.Storage["5"].GetPath());
+            if (ShortcutMgr.Storage.ContainsKey("6"))
+                SetApplicationIconFromPath(appBtn6, appImg6, appText6, ShortcutMgr.Storage["6"].GetPath());
         }
 
         private void WriteSettings()
         {
+            ShortcutMgr.WriteToStorage();
             if (SettingsChanged != null)
                 SettingsChanged(this, EventArgs.Empty);
         }
 
-        private void PickApplication(Button btn, System.Windows.Controls.Image img, TextBlock text)
+        private String PickApplication(Button btn, System.Windows.Controls.Image img, TextBlock text)
         {
             Microsoft.Win32.OpenFileDialog OFDialog = new Microsoft.Win32.OpenFileDialog();
             OFDialog.Multiselect = false;
             OFDialog.ShowDialog();
             if(System.IO.File.Exists(OFDialog.FileName))
                 SetApplicationIconFromPath(btn, img, text, OFDialog.FileName);
+            return OFDialog.FileName;
         }
 
         private void SetApplicationIconFromPath(Button btn, System.Windows.Controls.Image img, TextBlock text, String Path)
@@ -96,32 +107,38 @@ namespace JustLaunch
 
         private void appBtn1_Click(object sender, RoutedEventArgs e)
         {
-            PickApplication(appBtn1, appImg1, appText1);
+            String ExecPath = PickApplication(appBtn1, appImg1, appText1);
+            ShortcutMgr.Storage["1"] = new Shortcut(ExecPath);
         }
 
         private void appBtn2_Click(object sender, RoutedEventArgs e)
         {
-            PickApplication(appBtn2, appImg2, appText2);
+            String ExecPath = PickApplication(appBtn2, appImg2, appText2);
+            ShortcutMgr.Storage["2"] = new Shortcut(ExecPath);
         }
 
         private void appBtn3_Click(object sender, RoutedEventArgs e)
         {
-            PickApplication(appBtn3, appImg3, appText3);
+            String ExecPath = PickApplication(appBtn3, appImg3, appText3);
+            ShortcutMgr.Storage["3"] = new Shortcut(ExecPath);
         }
 
         private void appBtn4_Click(object sender, RoutedEventArgs e)
         {
-            PickApplication(appBtn4, appImg4, appText4);
+            String ExecPath = PickApplication(appBtn4, appImg4, appText4);
+            ShortcutMgr.Storage["4"] = new Shortcut(ExecPath);
         }
 
         private void appBtn5_Click(object sender, RoutedEventArgs e)
         {
-            PickApplication(appBtn5, appImg5, appText5);
+            String ExecPath = PickApplication(appBtn5, appImg5, appText5);
+            ShortcutMgr.Storage["5"] = new Shortcut(ExecPath);
         }
 
         private void appBtn6_Click(object sender, RoutedEventArgs e)
         {
-            PickApplication(appBtn6, appImg6, appText6);
+            String ExecPath = PickApplication(appBtn6, appImg6, appText6);
+            ShortcutMgr.Storage["6"] = new Shortcut(ExecPath);
         }
     }
 }
